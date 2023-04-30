3a9f1258aa6273301232c2a1f0a9f37d18ba5bf2
devsjuanh <jc.hernandezp1@uniandes.edu.co>
2023-04-30 18:20:35 -0500

    local changes

--- moviles_g13/app/src/main/java/com/example/moviles_g13/ui/albums_visitor/AlbumsVisitorViewModel.kt
+++ moviles_g13/app/src/main/java/com/example/moviles_g13/ui/albums_visitor/AlbumsVisitorViewModel.kt
@@ -9,15 +9,15 @@ class AlbumsVisitorViewModel : ViewModel() {
 
     private val _albums = MutableLiveData<List<Album>>().apply {
         val dataList: List<Album> = listOf<Album>(
-                Album(albumId=1, name="Camisa negra", cover="https://ibb.co/GTtGX4m", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
+                Album(albumId=1, name="Camisa negra", cover="https://weareipt.com/col.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
                 Album(albumId=1, name="Camisa negra", cover="https://2.bp.blogspot.com/-tzm1twY_ENM/XlCRuI0ZkRI/AAAAAAAAOso/BmNOUANXWxwc5vwslNw3WpjrDlgs9PuwQCLcBGAsYHQ/s1600/pasted%2Bimage%2B0.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
-                Album(albumId=1, name="Camisa negra", cover="https://en.wikipedia.org/wiki/La_Camisa_Negra#/media/File:La_Camisa_Negra.jpg", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
-                Album(albumId=1, name="Camisa negra", cover="https://ibb.co/GTtGX4m", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
+                Album(albumId=1, name="Camisa negra", cover="https://weareipt.com/usa.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
+                Album(albumId=1, name="Camisa negra", cover="https://weareipt.com/col.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
                 Album(albumId=1, name="Camisa negra", cover="https://2.bp.blogspot.com/-tzm1twY_ENM/XlCRuI0ZkRI/AAAAAAAAOso/BmNOUANXWxwc5vwslNw3WpjrDlgs9PuwQCLcBGAsYHQ/s1600/pasted%2Bimage%2B0.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
-                Album(albumId=1, name="Camisa negra", cover="https://en.wikipedia.org/wiki/La_Camisa_Negra#/media/File:La_Camisa_Negra.jpg", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
-                Album(albumId=1, name="Camisa negra", cover="https://ibb.co/GTtGX4m", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
+                Album(albumId=1, name="Camisa negra", cover="https://weareipt.com/usa.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
+                Album(albumId=1, name="Camisa negra", cover="https://weareipt.com/col.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
                 Album(albumId=1, name="Camisa negra", cover="https://2.bp.blogspot.com/-tzm1twY_ENM/XlCRuI0ZkRI/AAAAAAAAOso/BmNOUANXWxwc5vwslNw3WpjrDlgs9PuwQCLcBGAsYHQ/s1600/pasted%2Bimage%2B0.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony"),
-                Album(albumId=1, name="Camisa negra", cover="https://en.wikipedia.org/wiki/La_Camisa_Negra#/media/File:La_Camisa_Negra.jpg", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony")
+                Album(albumId=1, name="Camisa negra", cover="https://weareipt.com/usa.png", releaseDate="2021", description="Tengo la camisa negra de juanes", genre="POP", recordLabel="Sony")
         )
         value = dataList
     }
