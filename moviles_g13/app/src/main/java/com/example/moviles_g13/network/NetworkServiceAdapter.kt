7a476361292703c2e70f3ab28f710e804a94a4c5
devsjuanh <jc.hernandezp1@uniandes.edu.co>
2023-04-30 21:15:37 -0500

    implement with service

--- moviles_g13/app/src/main/java/com/example/moviles_g13/network/NetworkServiceAdapter.kt
+++ moviles_g13/app/src/main/java/com/example/moviles_g13/network/NetworkServiceAdapter.kt
@@ -8,6 +8,7 @@ import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.moviles_g13.model.Album
 import com.example.moviles_g13.model.Artist
 import org.json.JSONArray
 import org.json.JSONObject
@@ -49,6 +50,38 @@ class NetworkServiceAdapter constructor(context: Context) {
                                 albums = item.getJSONArray("albums"),
                                 performerPrizes = item.getJSONArray("performerPrizes"),
                                 description = item.getString("description")
+                            )
+                        )
+                    }
+                    onComplete(list)
+                },
+                Response.ErrorListener {
+                    onError(it)
+                })
+        )
+    }
+
+
+    fun getAlbums(
+        onComplete: (resp: List<Album>) -> Unit,
+        onError: (error: VolleyError) -> Unit
+    ) {
+        requestQueue.add(
+            getRequest("albums",
+                Response.Listener<String> { response ->
+                    val resp = JSONArray(response)
+                    val list = mutableListOf<Album>()
+                    for (i in 0 until resp.length()) {
+                        val item = resp.getJSONObject(i)
+                        list.add(
+                            i, Album(
+                                albumId = item.getInt("id"),
+                                name = item.getString("name"),
+                                cover = item.getString("cover"),
+                                releaseDate = item.getString("releaseDate"),
+                                description = item.getString("description"),
+                                genre = item.getString("genre"),
+                                recordLabel = item.getString("recordLabel")
                             )
                         )
                     }
