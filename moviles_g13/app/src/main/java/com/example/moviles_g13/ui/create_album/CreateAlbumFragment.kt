e024469d0494939f2b7beba43202811bef739a99
devsjuanh <jc.hernandezp1@uniandes.edu.co>
2023-05-14 09:45:19 -0500

    finishing implemntaciòn

--- moviles_g13/app/src/main/java/com/example/moviles_g13/ui/create_album/CreateAlbumFragment.kt
+++ moviles_g13/app/src/main/java/com/example/moviles_g13/ui/create_album/CreateAlbumFragment.kt
@@ -1,40 +1,31 @@
-package com.example.moviles_g13.ui.home_collector
+package com.example.moviles_g13.ui.create_album
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.navigation.fragment.findNavController
 import com.example.moviles_g13.R
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class HomeCollectorFragment : Fragment() {
+class CreateAlbumFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.collector_home_fragment, container, false)
+        return inflater.inflate(R.layout.create_album_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<ImageButton>(R.id.back_button).setOnClickListener {
-            findNavController().navigate(R.id.action_collector_to_home_layout)
-        }
-
-        view.findViewById<Button>(R.id.album_button).setOnClickListener {
-            findNavController().navigate(R.id.action_collector_to_create_album_layout)
+            findNavController().navigate(R.id.action_create_album_to_HomeCollectorFragment)
         }
 
     }
-
 
 }
